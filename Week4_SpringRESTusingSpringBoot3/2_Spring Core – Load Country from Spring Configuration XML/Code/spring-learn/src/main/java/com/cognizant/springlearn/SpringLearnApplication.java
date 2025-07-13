c0efea89f3847d8f1d1027dafbc8070f7da959c4
AdityaKulkarniXD <beastwolftheleader@gmail.com>
2025-07-13 16:56:29 +0530

    adding week 4 hands on

---
+++ Week4_SpringRESTusingSpringBoot3/2_Spring Core – Load Country from Spring Configuration XML/Code/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,26 @@
+package com.cognizant.springlearn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.util.List;
+
+
+public class SpringLearnApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+        displayCountries();
+    }
+
+    public static void displayCountries() {
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("country.xml")) {
+            @SuppressWarnings("unchecked")
+            List<Country> countryList = (List<Country>) context.getBean("countryList");
+            for (Country country : countryList) {
+                LOGGER.debug("Country : {}", country.toString());
+            }
+        }
+    }
+
+}
